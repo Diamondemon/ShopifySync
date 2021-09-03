ffdf8df60513786c5b658c07a2113cd320be5ed4
Julien M <julien.marzal@telecom-paris.fr>
2021-09-03 23:17:58 +0200

    Ajout de gestion d'erreurs.

--- set_tracking_number_service.rb
+++ set_tracking_number_service.rb
@@ -6,11 +6,11 @@ require 'ostruct'
 
 module ShopifySync
   class SetTrackingNumberService
-    attr_accessor :order, :fulfillment
+    attr_accessor :order, :fulfillment, :feedback
 
     def self.call(params)
       service = new(params)
-      service.fulfillment
+      service.feedback
     end
 
     private
@@ -19,6 +19,9 @@ module ShopifySync
       @tracking_number = params[:tracking_number]
       initiate_api(params[:login_info])
       getorder(params)
+    rescue TypeError
+      @feedback = OpenStruct.new('success?' => false, 'error_code' => 'Error on order.', 'data' => {order: @order})
+    else
       fill_fulfillment(params[:tracking_info])
       save_fulfillment
     end
@@ -31,16 +34,14 @@ module ShopifySync
         @line_items.append({ id: item.id, product_id: item.product_id,
                              variant_id: item.variant_id, quantity: item.fulfillable_quantity })
       end
-      raise @order unless order.success?
-
-      find_location
+      raise TypeError unless order.success?
     end
 
     def find_location
       variants = retrieve_variants
       variant = ShopifyAPI::Variant.find(variants[0])
-      inventory_lvl = ShopifyAPI::InventoryLevel.find(:all, params: { inventory_item_ids: variant.inventory_item_id })[0]
-      inventory_lvl.attributes[:location_id]
+      invent_lvl = ShopifyAPI::InventoryLevel.find(:all, params: { inventory_item_ids: variant.inventory_item_id })[0]
+      invent_lvl.attributes[:location_id]
     end
 
     def retrieve_variants
@@ -51,8 +52,7 @@ module ShopifySync
 
     def fill_fulfillment(tracking_info)
       @fulfillment = ShopifyAPI::Fulfillment.new
-      @fulfillment.attributes.merge!({ old_location_id: @order_items[0].origin_location.id,
-                                       location_id: find_location,
+      @fulfillment.attributes.merge!({ location_id: find_location,
                                        tracking_number: tracking_info[:tracking_number],
                                        tracking_url: tracking_info[:tracking_url],
                                        tracking_company: tracking_info[:tracking_company],
@@ -62,6 +62,25 @@ module ShopifySync
 
     def save_fulfillment
       @fulfillment.save
+    rescue ActiveResource::ResourceNotFound
+      error_code = 'Error code 404 Not Found; check the id of the order,'
+      error_code += ' verify that no variant was removed, or no location vas moved.'
+      @feedback = OpenStruct.new('success?' => false, 'error_code' => error_code, 'data' => nil)
+    else
+      verify_fulfillment
+    end
+
+    def verify_fulfillment
+      if @fulfillment.errors.nil?
+        @feedback = OpenStruct.new('success?' => true, 'error_code' => nil, 'data' => { fulfillment: @fulfillment })
+      elsif @fulfillment.errors.errors[0].type.scan('already fulfilled').empty?
+        @feedback = OpenStruct.new('success?' => false, 'error_code' => 'Unhandled error, see fulfillment object',
+                                   'data' => { fulfillment: @fulfillment })
+      else
+        error_code = 'Error 422, Unprocessable, items are already fulfilled'
+        @feedback = OpenStruct.new('success?' => false, 'error_code' => error_code,
+                                   'data' => { fulfillment: @fulfillment })
+      end
     end
 
     def initiate_api(login_info)
