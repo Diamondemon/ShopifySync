125c0c3f9f461ada14d35ade366a71ec0b6e0199
Julien M <julien.marzal@telecom-paris.fr>
2021-09-07 11:02:40 +0200

    Modification pour Rubocop

--- exceptions.rb
+++ exceptions.rb
@@ -4,7 +4,7 @@ module ShopifySync
   # Error when something bad happens when retrieving orders
   class SyncOrderError < RuntimeError
   end
-  
+
   # Error when the order is already fulfilled
   class AlreadyFulfilledError < RuntimeError
   end
